3d398c442276115caed25a566db6c72a417dcf85
NyRakath <Soncor23@gmail.com>
2021-11-09 10:26:20 -0500

    FIX sum of INT MAX and zero in d

--- specifiers_numbers.c
+++ specifiers_numbers.c
@@ -1,4 +1,18 @@
 #include "main.h"
+
+/**
+ * powfake - represent power of the number
+ * @num: Base number
+ * @powe: Power number
+ * Return: recursion of the funtion * num
+ */
+int powfake(int num, int powe)
+{
+	if (powe <= 0)
+		return (1);
+	else
+		return (num * powfake(num, powe - 1));
+}
 /**
  * specifier_d - Convert the argument into integers.
  * @ap: Pointer to the list of arguments to print.
@@ -6,35 +20,45 @@
  */
 int specifier_d(va_list ap)
 {
-	int digit, rev, d, size = 0;
-	int dval = va_arg(ap, int);
+	int last_digit, reversedn, first_number, savenumb, a, size = 0;
+	int n = va_arg(ap, int);
+	int i = 0;
 
-	if (dval < 0)
+	savenumb = n;
+	if (n < 0)
 	{
 		size += _putchar('-');
-		digit = ('0' - (dval % 10));
-		dval /= -10;
+		last_digit = ('0' - (n % 10));
+		n /= -10;
 	}
 	else
 	{
-		digit = ((dval % 10) + '0');
-		dval /= 10;
+		last_digit = ((n % 10) + '0');
+		n /= 10;
 	}
-	rev = 0;
-	for (; dval > 0;)
+	for (reversedn = 0; n > 0; i++)
 	{
-		rev = ((rev * 10) + (dval % 10));
-		dval /= 10;
+		reversedn = ((reversedn * 10) + (n % 10));
+		n /= 10;
+		if (n <= 9)
+		{
+			first_number = reversedn;
+		}
 	}
-	for (; rev > 0;)
+	for (; reversedn > 0;)
 	{
-		d = ((rev % 10) + '0');
-		size += _putchar(d);
-		rev /= 10;
-	}
-	size += _putchar(digit);
+		int d = ((reversedn % 10) + '0');
 
-	return (size);
+		size += _putchar(d);
+		reversedn /= 10;
+	}
+	_putchar(last_digit);
+	if (first_number == (savenumb / powfake(10, i)))
+	{
+		for (a = 0; a <= i - 2; a++)
+			size += _putchar('0');
+	}
+	return (savenumb);
 }
 /**
  * specifier_i - Convert the argument into integers.
