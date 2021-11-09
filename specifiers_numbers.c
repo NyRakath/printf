6900aae3c9a2e208377dea1247b3a2affbed0826
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-11-08 20:04:35 -0500

    Fix the recursion function

--- specifiers_numbers.c
+++ specifiers_numbers.c
@@ -6,10 +6,17 @@
  */
 int recursion_d(int n)
 {
-	if (n / 10 == 0)
-		return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (-(n));
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+		recursion_d(n / 10);
 	_putchar(n % 10 + '0');
-	return (recursion_d(n /= 10));
+	return (n);
 }
 /**
  * specifier_d - Convert the argument into integers.
@@ -20,8 +27,7 @@ int specifier_d(va_list ap)
 {
 	int dval = va_arg(ap, int);
 
-	recursion_d(dval);
-	return (0);
+	return (recursion_d(dval));
 }
 /**
  * specifier_i - Convert the argument into integers.
@@ -30,7 +36,8 @@ int specifier_d(va_list ap)
  */
 int specifier_i(va_list ap)
 {
-	(void)ap;
+	int dval = va_arg(ap, int);
 
+	recursion_d(dval);
 	return (0);
 }
